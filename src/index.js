cae66662f7e5fec1f6d247dd13483f7eb0ef2196
sayalikohad <sayalikoha3@gmail.com>
2023-10-11 14:03:38 +0530

    add contact.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Contact from './Views/Contact/Contact';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  
+    <Contact />
 );
 
 
