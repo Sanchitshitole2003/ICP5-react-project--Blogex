ec50ffc8d8a37295bb5a5fc4de3043fd51ea1364
sayalikohad <sayalikoha3@gmail.com>
2023-10-11 14:09:19 +0530

    add contact.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import Home from './view/home/home';
+import Contact from './Views/Contact/Contact';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 
-   <>
-  <Home/>
+  <>
+    <Contact />
+    <Home/>
   </>
 
 );
