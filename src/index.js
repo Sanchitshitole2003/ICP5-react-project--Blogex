fff858b0e80be03e13b5e89f2c05f7caece85ba3
sayalikohad <sayalikoha3@gmail.com>
2023-10-14 16:35:34 +0530

    add contact.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import Contact from './view/contact/contact';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const router = createBrowserRouter([
-  {
-    path: '/template',
-    element: <Template/>
-  },
-  {
-    path: '/home',
-    element: <Home/>
-  }])
 root.render(
   <Contact />
   
