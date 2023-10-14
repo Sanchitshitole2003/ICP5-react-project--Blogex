137c8f30fb2af107e5ab10459dd930a48cc78c08
noorshaikh123 <noaimcode@gmail.com>
2023-10-14 15:35:35 +0530

    made changes in home page

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,20 +1,20 @@
 import "./Navbar.css"
 import { Link } from "react-router-dom";
-import Icon from "./b.png"
+import logo from "./logo.png"
 
 function Navbar() {
   return (
     <nav className="navbar">
         <div className="brand_container">
-          <img src={Icon} alt="" className="icon"/>
-          <h1 className="navbar-title">Blogex</h1>
+          <img src={logo} alt="" className="icon"/>
+         <h1 className="navbar-title">Blogex</h1>
         </div>
         <div>
       <Link to="/" className="navbar_component">Home</Link>
       <Link to="/blog" className="navbar_component">Blog</Link>
+      <Link to="/template" className="navbar_component">Template</Link>
       <Link to="/about" className="navbar_component">About</Link>
       <Link to="/contact" className="navbar_component">Contact</Link>
-      <Link to="/template" className="navbar_component">Template</Link>
       <Link to="/Signup" className="navbar_component sign">SignUp</Link>
       </div>
     </nav>
--- src/view/home/home.js
+++ src/view/home/home.js
@@ -1,6 +1,6 @@
 import './home.css';
 import React, { useState, useEffect } from 'react';
-import image1 from './image1.png';
+import image1 from './image7.png';
 import image2 from './image3.png';
 import imaget1 from './imaget1.jpg';
 import Navbar from '../../components/Navbar/Navbar';
@@ -11,21 +11,33 @@ import Template from '../template/template';*/
 export default function Home() {
     return (
         <> <Navbar/>
-            <div className='parentdiv1'>
-                <div classname='containerdiv1'>
+        <div class="startdiv">
+            <br/><br/><br/><br/><br/><br/>
+         <h1 className='titlediv'>BLOGEX</h1>
+         <h3 className='titlesubdiv'>Express your knowledge and Experience</h3>
+        </div>
+            <div className='parentdiv1'> 
+                <div classname='container1'>
                     <h1 className='titlehome'>Create a blog</h1>
                     <div className='titletexthome'>Share your story with the world.
                         Stand out with a professionally-designed blog website that can be customized to fit your brand.
                         Build, manage, and promote your blog with blogex built-in suite of design and marketing tools.</div>
                     <button className='btnhm1'>Create Blog</button>
                 </div>
-                <div>
+
+               <div>
                     <img src={image1} className='imagediv1' />
                 </div>
             </div>
-
+             
             <div className='parentdiv2'>
                     <h1 className='titlehome2'>Explore<br/> Design options</h1>
+                    <div className="parent2text">
+                    Designing a blog involves considering both the visual and functional aspects of your website. 
+                    A well-designed blog should be visually appealing, easy to navigate, and user-friendly. 
+                    The design should enhance the content and help you achieve your blogging goals, 
+                    whether that's sharing information, building a community, or generating income through your blog.
+                    </div>
                     <div>
                         <img src={image2} className='imagediv2' />
                     </div>
@@ -35,11 +47,10 @@ export default function Home() {
                         <h2>Work From home</h2>
                         <h3>Work From home is an easy way to offer your availabilty without your physical presence in the office</h3> </div>
                     </div>
-
-                    <div className='titletexthome2'></div>
                     <button className='btnhm2'>Visit Template</button>
             </div>
-
+            
+            
             <div className='parentdiv3'>
                 <div className='creatediv'>
                     How to create a BLOG?
