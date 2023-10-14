c38c947725d2cb5c5b371687040e5c86bae34ff9
Hemant <hsgaikwad0015@gmail.com>
2023-10-14 11:24:05 +0530

    routing added in index.js file and fotter css

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,14 +5,21 @@ import Icon from "./b.png"
 function Navbar() {
   return (
     <nav className="navbar">
+      <div className="navbar_container">
         <div className="brand_container">
-          <img src={Icon} alt="" className="icon"/>
+          <img src={Icon} alt="" className="icon" />
           <h1 className="navbar-title">Blogex</h1>
         </div>
-      <Link to="/" className="navbar_component">Home</Link>
-      <Link to="/about" className="navbar_component">About</Link>
-      <Link to="/contact" className="navbar_component">Contact</Link>
-      <Link to="/blog" className="navbar_component">Blog</Link>
+        <div className="menu_container">
+          <Link to="/" className="navbar_component">Home</Link>
+          <Link to="/about" className="navbar_component">About</Link>
+          <Link to="/contact" className="navbar_component">Contact</Link>
+          <Link to="/blog" className="navbar_component">Blog</Link>
+        </div>
+        <div className="login_container">
+          <Link to="/login" className="navbar_component login">Login</Link>
+        </div>
+      </div>
     </nav>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from './view/home/home';
+import Contact from "./view/contact/contact";
+import About from "./view/about/about";
+import Blog from "./view/blogdisplay/blogdisplay";
+import BlogWriting from "./view/blogwriting/blogwriting";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,   
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/blogdisplay",
+    element: <Blog />,
+  },
+  {
+    path: "/blogwriting",
+    element: <BlogWriting />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <>
-  <Home/>
-  </>
+  <RouterProvider router={router} />
 );
 
 
